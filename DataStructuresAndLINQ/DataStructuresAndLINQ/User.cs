915b08b88a08cb6be5505aa4355353e6e4395f41
tetiana-zaichuk <tanyazaychuk@gmail.com>
2018-07-03 12:58:27 +0300

    Created basic classes and started making Queries

---
+++ DataStructuresAndLINQ/DataStructuresAndLINQ/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresAndLINQ
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+        public string Email { get; set; }
+    }
+}
